bf84309da9ee7fede03188a9a4fc2bacef301ea9
Emmanuel <tydevelopes@gmail.com>
2023-02-16 02:31:18 -0800

    Added script for task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a string
+ * Return: 1 if error
+ */
+int main(void)
+{
+	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = strlen(str);
+
+	fwrite(str, sizeof(char), len, stderr);
+
+	return 1;
+}
