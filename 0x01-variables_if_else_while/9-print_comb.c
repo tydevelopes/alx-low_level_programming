0028bcf5c87bf34a162c3b455c74c9d1b32e10ca
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:55:13 -0800

    Added script for task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * main - prints 0 to 9 using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int i = 48;
+	int nine = 57;
+
+	while (i <= nine)
+	{
+		putchar(i);
+		if (i != nine)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
