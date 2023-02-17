df649508e8f64e3314498f9f1d6c26c975fd5e70
Emmanuel <tydevelopes@gmail.com>
2023-02-17 15:25:45 -0800

    Added script for task 12

---
+++ 0x01-variables_if_else_while/102-print_combs.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if(!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
