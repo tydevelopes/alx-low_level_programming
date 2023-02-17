41a0a7c659452743f9b848ba956c786cb77acbac
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:20:01 -0800

    Added script for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf('\n');
+
+	return (0);
+}
