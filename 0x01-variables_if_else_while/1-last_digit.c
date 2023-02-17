83bc9c638edaf92e6ed54d9bc8ccfcef03ec7ceb
Emmanuel <tydevelopes@gmail.com>
2023-02-17 04:40:55 -0800

    Fixed betty stle error in script 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
 
 	if (d > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
-	else if (d == 0)
+	if (d == 0)
 		printf("Last digit of %d is %d and is 0\n", n, d);
-	else if (d < 6)
+	if (d < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 
 	return (0);
