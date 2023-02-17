6ca92e99ed0f4bdc787d6f6461418153956d60e3
Emmanuel <tydevelopes@gmail.com>
2023-02-17 02:51:48 -0800

    Added script for task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * main - prints all possible different combinations of three digits using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int i = 48;
+	int nine = 57;
+	int j;
+	int k;
+
+	while (i <= nine)
+	{
+		j = i;
+		while (j <= nine)
+		{
+			k = j;
+			while (k <= nine)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == nine - 2 && j == nine - 1 && k == nine))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
