227d787259bd9df1f11f98e529df97eade664fa8
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:25:49 -0800

    Added script for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - prints 0 to 9 using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int i = 48;
+	int nine = 57;
+
+	while (i <= nine)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
