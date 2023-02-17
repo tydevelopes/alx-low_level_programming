3948c1f62aae65072cc40738e60db19a82595834
Emmanuel <tydevelopes@gmail.com>
2023-02-17 00:24:37 -0800

    Added script for task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - checks random number if it is positive, zero, or negative
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0) 
+		printf("%d is positive\n");
+	else if (n == 0)
+		printf("%d is zero\n");
+	else
+		printf("%d is negative\n");
+
+	return (0);
+}
