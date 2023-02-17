3aea41aeef0b13f22d6d709b8f79d7cd5723e598
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:39:49 -0800

    Added script for task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * main - prints all numbers of base 16 in lowercase and using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int i = 48;
+	int nine = 57;
+	int j = 97;
+	int f = 102;
+
+	while (i <= nine)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j <= f)
+        {
+                putchar(j);
+                j++;
+        }
+
+	putchar('\n');
+
+	return (0);
+}
