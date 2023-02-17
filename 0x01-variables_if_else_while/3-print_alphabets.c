73e580f7c415cbc9f3aede3d6d25ca7e7f7be6fc
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:05:42 -0800

    Added script for task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets in lowercase and uppercase using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int a = 97;
+	int z = 122;
+	int A = 65;
+	int Z = 90;
+
+	while (a <= z)
+	{
+		putchar(a);
+		a++;
+	}
+
+	while (A <= Z)
+        {
+                putchar(A);
+                A++;
+        }
+
+	putchar('\n');
+
+	return (0);
+}
