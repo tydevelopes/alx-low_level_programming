b4394036fe33cf945db5caeb49e718cf2553905f
Emmanuel <tydevelopes@gmail.com>
2023-02-17 01:13:29 -0800

    Added script for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets in lowercase, except q and e, using ASCII codes
+ * Return - 0 if success
+ */
+int main(void)
+{
+	int a = 97;
+	int z = 122;
+	int e = 101;
+	int q = 113;
+
+	while (a <= z)
+	{
+		if (a == e || a == q)
+		{
+			a++;
+			continue;
+		}	
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
