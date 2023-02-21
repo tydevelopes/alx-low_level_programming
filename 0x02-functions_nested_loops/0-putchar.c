c8572ab6baf41a0d108e35d720915579e79c7761
Emmanuel <tydevelopes@gmail.com>
2023-02-20 22:31:53 -0800

    Added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks last digit random number if it is > 5, 0 or < 6
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,7 @@
 # 0x02. C -Functions and nested loops
+
+## Tasks and a brief description of their functions
+
+### 0-putchar.c
+Print  the word _putchar followed by newline.
+
