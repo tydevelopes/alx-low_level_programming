924e758234860405a2b6e50ae8a72924fb716126
Emmanuel <tydevelopes@gmail.com>
2023-02-21 21:13:52 -0800

    Added task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is zero
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,7 @@
+# 0x03-debugging
+
+## Tasks and the functions
+
+### 0-main.c
+test that the function positive_or_negative() gives the correct output when
+given a case of 0
