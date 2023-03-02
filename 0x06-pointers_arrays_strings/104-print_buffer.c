d254e12e9c43bfc1bb6af3d2e40ddcf14c099d3d
Emmanuel <tydevelopes@gmail.com>
2023-03-02 01:31:46 -0800

    Fixed errors in task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <ctype.h>
+#include <stdio.h>
 
 /**
  * print_buffer - prints a buffer
@@ -7,5 +9,43 @@
  */
 void print_buffer(char *b, int size)
 {
-	printf("Todo\n");
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x ", i);
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+				printf("%02x", b[j] & 0xff);
+			else
+				printf("  ");
+			if (j + 1 < size)
+				printf("%02x", b[j + 1] & 0xff);
+			else
+				printf("  ");
+			printf(" ");
+		}
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+			{
+				if (isprint(b[j]))
+					printf("%c", b[j]);
+				else
+					printf(".");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+			printf("\n");
+	}
 }
