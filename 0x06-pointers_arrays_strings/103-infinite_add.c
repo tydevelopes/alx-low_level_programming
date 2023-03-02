cace49a8972987197b717eff148cfb3820cd0949
Emmanuel <tydevelopes@gmail.com>
2023-03-02 01:06:01 -0800

    Fixed error in task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * infinite_add - add two numbers
@@ -10,5 +11,28 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	return (r);
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int max_len = len1 > len2 ? len1 : len2;
+	int carry = 0;
+	int sum = 0;
+	int i;
+
+	if (max_len + 1 > size_r)
+		return (0);
+
+	for (i = 0; i < max_len || carry != 0; i++)
+	{
+		sum = carry;
+		if (i < len1)
+			sum += n1[len1 - 1 - i] - '0';
+		if (i < len2)
+			sum += n2[len2 - 1 - i] - '0';
+		carry = sum / 10;
+		sum %= 10;
+		r[max_len - i] = sum + '0';
+	}
+	if (r[0] == '\0')
+		return (0);
+	return (&r[max_len - i + 1]);
 }
