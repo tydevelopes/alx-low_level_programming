9e480c47abd83ee5080ea16b2956fe5ace601964
Emmanuel <tydevelopes@gmail.com>
2023-03-10 17:50:26 -0800

    Fixed task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+int has_nondigit(char *s);
 
 /**
  * main - add positive numbers
@@ -17,7 +20,7 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		n = atoi(argv[i]);
-		if ((!isdigit(argv[i][0]) && !n) || n < 0)
+		if (has_nondigit(argv[i]) || n < 0)
 		{
 			printf("Error\n");
 			return (1);
@@ -26,5 +29,23 @@ int main(int argc, char *argv[])
 	}
 	printf("%d\n", sum);
 
+	return (0);
+}
+
+/**
+ * has_nondigit - checks for non digit
+ * @s: pointer to the string to check
+ * Return: 1 if true, 0 otherwise
+ */
+int has_nondigit(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (isalpha(s[i]))
+			return (1);
+	}
 	return (0);
 }
