480aae09d1fe2adf64badbfe4ea1fbc63b5e22d2
Emmanuel <tydevelopes@gmail.com>
2023-03-22 16:57:29 -0700

    Fixed error in task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,14 +13,13 @@ int (*get_op_func(char *s))(int, int)
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+		{"%", op_mod}
 	};
 	int i;
 
 	i = 0;
 
-	while (i < 6)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
