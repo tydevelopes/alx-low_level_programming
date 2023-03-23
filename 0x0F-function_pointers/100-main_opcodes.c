12baa770c3b39e30d5568d861fa7f6b6bfb58f6f
Emmanuel <tydevelopes@gmail.com>
2023-03-22 17:44:29 -0700

    Fixed error in task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,15 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
+	int bytes, i;
+	char *p;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	p = (char *) main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *(p + i));
+
+		if (i != bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+
 	return (0);
 }
