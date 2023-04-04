e945607d3b54b756c7167bc2427101c4b4724e12
Emmanuel <tydevelopes@gmail.com>
2023-04-04 00:13:20 -0700

    Added task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all elements of a listint_t list
+ * @head: pointer to the list
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t length = 0;
+	const listint_t *current_node = NULL;
+	const listint_t *array[1024] = {NULL};
+
+	if (!head)
+		exit(98);
+
+	current_node = head;
+
+	while (current_node)
+	{
+		if (contains_address(array, current_node))
+			break;
+		array[length] = current_node;
+
+		length++;
+		printf("[%p] %d\n", (void *)current_node ,current_node->n);
+		current_node = current_node->next;
+	}
+	return (length);
+}
+
+/**
+ * contains-address - checks if array contains given node
+ * @array: array of pointers to listint_t
+ * @node: node to search
+ * Return: 1 if found, 0 otherwise
+ */
+int contains_address(const listint_t *array[], const listint_t *node)
+{
+	int i = 0;
+
+	while (array[i])
+	{
+		if (array[i] == node)
+			return (1);
+		i++;
+	}
+	return (0);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int contains_address(const listint_t *array[], const listint_t *node);
 
 #endif /* lists_h */
