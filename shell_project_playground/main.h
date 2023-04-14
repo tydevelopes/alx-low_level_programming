f9b52758caea57c99c0f103b5c2055625c96980a
Emmanuel <tydevelopes@gmail.com>
2023-04-14 05:24:25 -0700

    Refactored super simple shell

--- shell_project_playground/main.h
+++ shell_project_playground/main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 extern char **environ;
 
---
+++ shell_project_playground/super_simple_shell_v1.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * main - super simple shell
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int status, i = 0;
+	FILE *stream = stdin;
+	char *line = NULL, *path_to_exec = NULL;
+	size_t len = 0;
+	ssize_t bytes_read = 0;
+
+	printf("#cisfun$ ");
+	while ((bytes_read = getline(&line, &len, stream)) != -1)
+	{
+		/* remove newline */
+		path_to_exec = malloc(sizeof(char) * strlen(line));
+		i = 0;
+		while (line[i] != '\n')
+		{
+			path_to_exec[i] = line[i];
+			i++;
+		}
+		path_to_exec[i] = '\0';
+
+		char *argv[] = {path_to_exec, NULL};
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror(argv[0]);
+				printf("#cisfun$ ");
+			}
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				perror("Error");
+				return (1);
+			}
+			printf("#cisfun$ ");
+		}
+	}
+	free(line);
+	fclose(stream);
+	return (0);
+}
