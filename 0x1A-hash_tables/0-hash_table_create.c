325f666b8cd64d440f5c483f3ebe90e323615ad3
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-06-09 01:19:40 +0000

    fixed errors in task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 	unsigned long int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+	/* Allocate memory for hash node items */
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 
+	/* Initialise hash node items to NULL */
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
+
+	table->size = size;
 
 	return (table);
 }
