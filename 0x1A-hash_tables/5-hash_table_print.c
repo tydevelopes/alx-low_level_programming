457195d2f6e65c2da4ba57bad30aca388e4ef3a3
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-06-09 07:44:29 +0000

    added task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: pointer to the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	int first_print = 1;
+	hash_node_t *node = NULL;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+
+		if (node)
+		{
+			if (first_print)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				first_print = 0;
+			}
+			else
+			{
+				printf(", '%s': '%s'", node->key, node->value);
+			}
+		}
+	}
+	printf("}\n");
+}
